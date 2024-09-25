f1260dcb77438911e2bef016805920b32bbfdb2c
M Jeevan <davidjeevan08@gmail.com>
2024-09-26 02:28:09 +0530

    removed blue color from the site and added red color in the background

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import Explore from './pages/Explore';
 import Confess from './pages/Confess';
 import Communities from './pages/Communities';
-import './App.css'; // Import the CSS file for styling
+import './App.css'; // Import the updated CSS file for styling
 
 function App() {
   // Start with an empty posts array instead of hardcoded confessions
@@ -31,21 +31,29 @@ function App() {
           </div>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
+              {/* Updated Order */}
               <li><Link to="/explore">Explore</Link></li>
               <li><Link to="/confess">Confess</Link></li>
               <li><Link to="/communities">Communities</Link></li>
+              <li><Link to="/">Home</Link></li>
             </ul>
           </nav>
         </header>
 
         {/* Page Routes */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/explore" element={<Explore posts={posts} />} />
-          <Route path="/confess" element={<Confess addPost={addPost} />} />
-          <Route path="/communities" element={<Communities />} />
-        </Routes>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/explore" element={<Explore posts={posts} />} />
+            <Route path="/confess" element={<Confess addPost={addPost} />} />
+            <Route path="/communities" element={<Communities />} />
+          </Routes>
+        </div>
+
+        {/* Optional Footer */}
+        <footer>
+          <p>&copy; {new Date().getFullYear()} College Stories</p>
+        </footer>
       </div>
     </Router>
   );
